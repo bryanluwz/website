8f51472188b9e4221cbb680d17166b16dc626c06
Bryan <bryanlu.my@gmail.com>
2025-03-15 15:42:10 +0800

    fe minor changes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,22 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
-import App from "..";
+import App from "./pages/index";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
-import { AuthProvider } from "./context/AuthContext";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </ThemeProvider>
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ThemeProvider>
   );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,27 +1,8 @@
 import React from "react";
-import { AuthContext } from "../context/AuthContext";
-import { ChatPage } from "./ChatPage";
-import { SignInPage } from "./SignInPage";
 import { Box, CircularProgress } from "@mui/material";
 
-export const MainPage: React.FC = () => {
-  const { auth } = React.useContext(AuthContext);
-
-  // If have token and is loading, then show loading spinner
-  // If authenticate alreadt, then show ChatPage
-  // Else, show SignInPage
-
-  return (
-    <>
-      {auth.user ? (
-        <ChatPage />
-      ) : auth.token ? (
-        <Box sx={{ display: "flex" }}>
-          <CircularProgress />
-        </Box>
-      ) : (
-        <SignInPage />
-      )}
-    </>
-  );
+export const App: React.FC = () => {
+  return <>test</>;
 };
+
+export default App;
