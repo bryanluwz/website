c827441c7ccb99d1e98a8b943c307a0676d0bbe0
Bryan <bryanlu.my@gmail.com>
2025-04-22 16:21:41 +0800

    oopss forgot to change the server url + add locales

--- frontend/src/apis/urls.ts
+++ frontend/src/apis/urls.ts
@@ -1,3 +1,3 @@
-// export const chatbotUrl =
-// "https://personal-website-chatbot-worker.bryanlu-my.workers.dev/";
-export const chatbotUrl = "http://localhost:8787/"; // Local development URL
+export const chatbotUrl =
+  "https://personal-website-chatbot-worker.bryanlu-my.workers.dev/";
+// export const chatbotUrl = "http://localhost:8787/"; // Local development URL
